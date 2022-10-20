5f93fb99bc2e561f378e73551ce91e936ed71d3b
eunki0916 <110226511+eunki0916@users.noreply.github.com>
2022-10-20 14:29:16 +0900

    Add files via upload

--- js/sub.js
+++ js/sub.js
@@ -1,58 +1,6 @@
-
-let head_m = document.getElementById('top');
-let img = document.querySelector('.logo_img');
-let gnb = document.querySelector('.gnb');
-let login = document.querySelector('.login_login');
-let search = document.querySelector('.login_search');
 let menu = document.querySelector('.login_menu');
-let lang = document.querySelector('.lang');
-let header_bg = document.querySelector('.header_bg');
 let menu_close = document.querySelector('.menu_close');
-/* 헤더 스크롤 */
-window.addEventListener('scroll', e => {
-
-    if (document.documentElement.scrollTop > 50) {
-        img.setAttribute('src', 'img/top_logo2.png')
-        head_m.classList.add('top_scroll');
-        gnb.classList.add('header_wrap_scroll');
-        login.setAttribute('src', 'img/icon/ico_top_login.png')
-        search.setAttribute('src', 'img/icon/ico_top_search.png')
-        menu.setAttribute('src', 'img/icon/ico_top_allmenu.png')
-        lang.classList.add('header_wrap_scroll');
-        header_bg.classList.add('header_bg_scroll');
-    } else {
-        img.setAttribute('src', 'img/top_logo1.png')
-        head_m.classList.remove('top_scroll');
-        gnb.classList.remove('header_wrap_scroll');
-        login.setAttribute('src', 'img/ico_top_login.png')
-        search.setAttribute('src', 'img/ico_top_search.png')
-        menu.setAttribute('src', 'img/ico_top_allmenu.png')
-        lang.classList.remove('header_wrap_scroll');
-        header_bg.classList.remove('header_bg_scroll');
-    }
-});
-/*헤더 마우스*/
-head_m.addEventListener('mouseover', () => {
-    img.setAttribute('src', 'img/top_logo2.png')
-    head_m.classList.add('top_scroll');
-    gnb.classList.add('header_wrap_scroll');
-    login.setAttribute('src', 'img/icon/ico_top_login.png')
-    search.setAttribute('src', 'img/icon/ico_top_search.png')
-    menu.setAttribute('src', 'img/icon/ico_top_allmenu.png')
-    lang.classList.add('header_wrap_scroll');
-    header_bg.classList.add('header_bg_scroll');
-});
-head_m.addEventListener('mouseout', () => {
-    img.setAttribute('src', 'img/top_logo1.png')
-    head_m.classList.remove('top_scroll');
-    gnb.classList.remove('header_wrap_scroll');
-    login.setAttribute('src', 'img/ico_top_login.png')
-    search.setAttribute('src', 'img/ico_top_search.png')
-    menu.setAttribute('src', 'img/ico_top_allmenu.png')
-    lang.classList.remove('header_wrap_scroll');
-    header_bg.classList.remove('header_bg_scroll');
-});
-/* 메뉴 클릭 */ 
+/* 메뉴 클릭 */
 menu.onclick = function () {
     document.querySelector('body').style.overflow = "hidden";
     document.querySelector('.dark').style.display = "block";
@@ -78,24 +26,8 @@ reserve_click.forEach(function (aa, bb) {
         num = bb;
     }
 });
-
-/*푸터*/
-let arr = document.querySelector('.arr');
-let m_footer = document.querySelector('.m_footer_bot');
-
-arr.onclick = function () {
-    if (num == 0) {
-        arr.style.rotate = "180deg";
-        m_footer.classList.add('block');
-        num = 1;
-    } else {
-        arr.style.rotate = "0deg";
-        m_footer.classList.remove('block');
-        num = 0;
-    }
-}
-
-/* 모바일 헤더 */ 
+/*모바일 전체메뉴*/
+/* 모바일 헤더 */
 let service_trip_arr = document.querySelector('.service_trip_arr');
 let service_trip = document.querySelector('.service_trip');
 let service_air_arr = document.querySelector('.service_air_arr');
@@ -151,4 +83,19 @@ service_faq_arr.onclick = function () {
 service_coupon_arr.onclick = function () {
     event_coupon.classList.toggle('block');
     service_coupon_arr.classList.toggle('updown');
+}
+/*모바일 푸터*/
+let arr = document.querySelector('.arr');
+let m_footer = document.querySelector('.m_footer_bot');
+
+arr.onclick = function () {
+    if (num == 0) {
+        arr.style.rotate = "180deg";
+        m_footer.classList.add('block');
+        num = 1;
+    } else {
+        arr.style.rotate = "0deg";
+        m_footer.classList.remove('block');
+        num = 0;
+    }
 }
